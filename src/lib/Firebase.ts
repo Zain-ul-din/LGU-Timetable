50f4f90fb52d0a62c9bd063711208b5b2af73365
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-11-03 19:26:13 +0500

    firebase init

--- src/lib/Firebase.ts
+++ src/lib/Firebase.ts
@@ -1,1 +1,27 @@
-export default {}
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+import type { FirebaseApp } from 'firebase/app'
+import type { Auth } from 'firebase/auth'
+import type { Firestore }  from 'firebase/firestore'
+
+interface InitializeFirebaseAppType
+{
+    firebaseApp: FirebaseApp,
+    firebaseAuth: Auth,
+    firebaseStore: Firestore
+}
+
+export function initializeFirebaseApp () : InitializeFirebaseAppType
+{
+    const firebaseApp: FirebaseApp = initializeApp ()
+    const firebaseAuth: Auth = getAuth (firebaseApp)
+    const firebaseStore: Firestore = getFirestore (firebaseApp)
+
+    return {
+        firebaseApp,
+        firebaseAuth,
+        firebaseStore
+    }
+}

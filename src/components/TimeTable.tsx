d0d16f120df320df945d785c080834b6c9fbbf52
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-11-06 06:45:39 +0500

    overflow fix

--- src/components/TimeTable.tsx
+++ src/components/TimeTable.tsx
@@ -23,7 +23,7 @@ export default function TimeTable (
 
     return (
         <>
-            <TableStyle.TableContainer my = {5} p = {2}>
+            <TableStyle.TableContainer my = {5} p = {2} overflowX = {'scroll'} >
                 <TableStyle.Table variant= {'striped'} size={'sm'} p = {1}>
                     
                     {/* Head */}

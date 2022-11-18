db15b78957a5822cb8b9a445193d24a33df658a4
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-11-19 02:27:34 +0500

    main page

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,30 +1,36 @@
 import React from "react"
 import { useContext } from "react"
+import { TimeTableContext } from "../Hooks/TimeTableContext"
 
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
 import Hero from "../components/Hero"
 import Selection from "../components/Selection"
+import Loader from "../components/internals/Loader"
+import TimeTable from "../components/TimeTable"
+
+import { tableHeadTiles } from "../constants/Constants"
 
 interface MainPropsType
 {
-    showHeroComponent: boolean
+    showHeroComponent: boolean,
+    isLoading: boolean
 }
 
 export function Main (
-    {showHeroComponent}: MainPropsType 
+    {showHeroComponent, isLoading}: MainPropsType 
 ): JSX.Element
 {
+    const timeTableData = useContext (TimeTableContext)
 
     return (
         <>
             <NavBar />
             {showHeroComponent && <Hero />}
             {metaData == null ? <Loader isLoading = {true}/> : <Selection metaData={metaData}/>}
-            {timeTableData.loadingState ? 
-              <Loader isLoading = {true}/> : 
-              <>
-                {timeTableData.data && <TimeTable data = {timeTableData.data} headTitles = {tableHeadTiles}/>}
+            {timeTableData?.timeTableData.loadingState ? <Loader isLoading = {true}/> : 
+            <>
+                {timeTableData?.timeTableData.data && <TimeTable data = {timeTableData.timeTableData.data} headTitles = {tableHeadTiles}/>}
             </>}
             <Footer />
         </>

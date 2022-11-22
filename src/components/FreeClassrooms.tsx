2b842b2292961ef225305a415bfe4c26e23e5677
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-11-22 08:37:11 +0500

    classrooms loop over

--- src/components/FreeClassrooms.tsx
+++ src/components/FreeClassrooms.tsx
@@ -3,15 +3,15 @@ import { serverURL } from "../constants/Constants"
 import { useEffect, useState } from "react"
 import Loader from "./internals/Loader"
 
-export default function FreeClassrooms ()
+export default function FreeClassrooms (): JSX.Element
 {
-    const [loading, setLoading] = useState (true)
+    const [loading, setLoading] = useState <boolean>(true)
+    const [data, setData] = useState <any> (null)
 
     useEffect (()=>{
         let currDate:Date = new Date ()
         let hours:string = `${currDate.getHours ()}`.padStart (2, '0')
         let min: string = currDate.getMinutes () >= 30 ? '30' : '00'
-        // `${serverURL}/freerooms/?day=${currDate.getDay ()}&time=${hours}:${min}`
         useTalkToServer (`${serverURL}/freerooms/?day=${currDate.getDay ()}&time=${hours}:${min}` )
         .then (data=> {
             setLoading (false)
@@ -22,6 +22,12 @@ export default function FreeClassrooms ()
     return (
         <>
             {loading? <Loader isLoading = {true}/>  : <>comming soon...</>}
+            <>
+                Apis Response: 
+                {data && data.map ((ele: any, idx: number)=>{
+                    
+                })}
+            </>
         </>
     )
 }

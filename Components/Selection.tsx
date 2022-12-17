de4f12ea098acbbdafaed3fad4d9b8b7900c87a1
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-12-17 18:14:59 +0500

    framer motion

--- Components/Selection.tsx
+++ Components/Selection.tsx
@@ -1,26 +1,35 @@
+import Style from '../styles/Home.module.css';
+import { motion } from 'framer-motion';
 /*Helper Functions */
 
-function Card ({title}: {title:string}) {
+function Card({ title }: { title: string }) {
     return (
         <>
-            <li className={`bg-slate-300 shadow-xl rounded-lg`}>
+            <motion.li
+                className={`bg-slate-300 shadow-xl rounded-lg cursor-pointer ${Style.selection}`}
+                whileHover={{ scale: 1.05 }}
+                transition={{ type: 'tween' }}
+            >
                 <div className={`h-48 flex justify-center align-middle p-4`}>
                     <h1 className={`text-xl`}>{title}</h1>
                 </div>
-            </li>
+            </motion.li>
         </>
-    )
+    );
 }
 
 export default function Selection() {
     return (
         <div className={`lg:px-60 md:px-24 sm:px-16 lg:p-8 md:p-4 sm:p-2 base:p-2`}>
-            <ul className={`grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-2 base:grid-cols-1 gap-5`}>
-                {['Time Table', 'Free Classrooms', 'About us', 'Hall of fame'].map ((title:string,key:number)=>
-                    <Card key ={key} title={title}/>
+            <ul
+                className={`grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-2 base:grid-cols-1 gap-5`}
+            >
+                {['Time Table', 'Free Classrooms', 'About us', 'Hall of fame'].map(
+                    (title: string, key: number) => (
+                        <Card key={key} title={title} />
+                    )
                 )}
             </ul>
         </div>
-    )
+    );
 }
-

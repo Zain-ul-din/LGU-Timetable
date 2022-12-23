7250cbf5131b9b4ed0a0ea20da5faa59939585a7
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-12-23 10:31:47 +0500

    type aliases

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import ProgressBar from '@badrap/bar-of-progress';
 import Router from 'next/router';
@@ -7,19 +7,19 @@ const progress = new ProgressBar({
     size: 4,
     color: '#60a5fa',
     className: 'bar-of-progress',
-    delay: 0,
-})
-  
+    delay: 0
+});
+
 // this fixes safari jumping to the bottom of the page
 // when closing the search modal using the `esc` key
 if (typeof window !== 'undefined') {
-  progress.start()
-  progress.finish()
+    progress.start();
+    progress.finish();
 }
- 
-Router.events.on('routeChangeStart', () => progress.start())
-Router.events.on('routeChangeComplete', () => progress.finish())
-Router.events.on('routeChangeError', () => progress.finish())
+
+Router.events.on('routeChangeStart', () => progress.start());
+Router.events.on('routeChangeComplete', () => progress.finish());
+Router.events.on('routeChangeError', () => progress.finish());
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Header from '../components/Header';
-import Selection from '../components/Selection';
-import Footer from '../components/Footer';
+import Header from '@/components/Header';
+import Selection from '@/components/Selection';
+import Footer from '@/components/Footer';
 
 export default function Home() {
     return (

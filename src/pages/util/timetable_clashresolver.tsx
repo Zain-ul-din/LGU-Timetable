c7a8ba4de8c9dbe6550aaf9868c69cd7dd8492cd
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-01 16:43:57 +0500

    added: Head tag

--- src/pages/util/timetable_clashresolver.tsx
+++ src/pages/util/timetable_clashresolver.tsx
@@ -1,6 +1,8 @@
 import { getDocs } from "firebase/firestore";
 import { GetStaticPropsContext } from "next";
+import Head from "next/head";
 import TimetableClashResolver from "~/components/clash_resolver/TimetableClashResolver";
+import { SocialLinks } from "~/components/seo/Seo";
 import { timeTableCol } from "~/lib/firebase";
 import { TimetableDocType } from "~/types/typedef";
 
@@ -28,7 +30,23 @@ export default function Page ({
 }: {
     timetables: Array<TimetableDocType>
 }) {
-    return <TimetableClashResolver 
-        timetables = {timetables}
-    />
+
+    return <>
+        <Head>
+        <title>Timetable Clash Reslover</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta
+                name="description"
+                content="A non-official blazingly 🔥 fast website to access the LGU timetable and lgu timetable developer APIS. Welcome to our Timetable Clash Resolution Tool, designed exclusively for students like you, navigating the maze of course selection at our university."
+            />
+            <meta
+                name="keywords"
+                content={`LGU timetable, lgu time table, lgu, lgu class time table, non official lgu time table, fast lgu timetable, new lgu timetable, lgu new timetable, lgu better timetable, lgu timetable live, lgu free classes, lahore garrison university timetable, lahore garrison university new timetable, lahore garrison university fast timetable, lgu api, lgu developer apis, free classrooms`}
+            />
+            <SocialLinks />
+        </Head>
+        <TimetableClashResolver 
+            timetables = {timetables}
+        />
+    </>
 }

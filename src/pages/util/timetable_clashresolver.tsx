8ef8f632f309a582b80025353d86c902086803be
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-03 21:50:39 +0500

    migrate: to server side caching

--- src/pages/util/timetable_clashresolver.tsx
+++ src/pages/util/timetable_clashresolver.tsx
@@ -1,36 +1,9 @@
-import { getDocs } from "firebase/firestore";
-import { GetStaticPropsContext } from "next";
 import Head from "next/head";
 import TimetableClashResolver from "~/components/clash_resolver/TimetableClashResolver";
 import { SocialLinks } from "~/components/seo/Seo";
 import { FIREBASE_ANALYTICS_EVENTS, useFirebaseAnalyticsReport } from "~/lib/FirebaseAnalysis";
-import { timeTableCol } from "~/lib/firebase";
-import { TimetableDocType } from "~/types/typedef";
 
-/**
- * TODOS:
- *      Use on demand ISR(Incremental Static regeneration)
- *      SOURCE: https://nextjs.org/docs/pages/building-your-application/data-fetching/incremental-static-regeneration
- */
-export async function getStaticProps(context: GetStaticPropsContext) {
-    const timetableDocSpanShot = await getDocs(timeTableCol);
-
-    const timetables: Array<TimetableDocType> = timetableDocSpanShot.docs.map((timetable) =>
-        ({id: timetable.id, ...timetable.data()})
-    ) as Array<TimetableDocType>;
-    
-    return {
-        props: {
-            timetables
-        }
-    };
-}
-
-export default function Page ({
-    timetables
-}: {
-    timetables: Array<TimetableDocType>
-}) {
+export default function Page () {
 
     useFirebaseAnalyticsReport(FIREBASE_ANALYTICS_EVENTS.clash_resolver);
     
@@ -48,8 +21,6 @@ export default function Page ({
             />
             <SocialLinks />
         </Head>
-        <TimetableClashResolver 
-            timetables = {timetables}
-        />
+        <TimetableClashResolver />
     </>
 }

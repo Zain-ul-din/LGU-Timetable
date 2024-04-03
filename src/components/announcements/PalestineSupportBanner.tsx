49d136235712b48fd9cb7d7d925fac81791df67b
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-04-03 21:46:34 +0500

    feat: added don8 palestine

--- src/components/announcements/PalestineSupportBanner.tsx
+++ src/components/announcements/PalestineSupportBanner.tsx
@@ -1,14 +1,86 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, useMediaQuery } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
-
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 // [![ReadMeSupportPalestine]()](https://github.com/Safouene1/support-palestine-banner)
 
 interface PalestineSupportBannerProps {
   hideMessage?: boolean;
 }
 
+const quranicVerses = [
+  'For those who give in charity, men and women, and loan to Allah a beautiful loan, it shall be increased manifold (to their credit), and they shall have (besides) a liberal reward.',
+  'The example of those who spend their wealth in the way of Allah is like a seed [of grain] which grows seven spikes; in each spike is a hundred grains. And Allah multiplies [His reward] for whom He wills. And Allah is all-Encompassing and Knowing.',
+  'You will never attain righteousness until you spend in charity from that what you love.'
+];
+
 export default function PalestineSupportBanner(props: PalestineSupportBannerProps) {
+  const [isUnder600] = useMediaQuery('(max-width: 600px)');
+  const [verseIndex] = useState(() => (Math.random() * quranicVerses.length) | 0);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      style={{
+        width: '100%',
+        padding: '0.4rem 0.5rem',
+        background: '#111'
+      }}>
+      <a
+        href="https://alkhidmat.org/appeal/emergency-appeal-palestine-save-lives-in-gaza-today"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          gap: '0.3rem'
+        }}>
+        <div
+          style={{
+            display: 'flex',
+            gap: '0.5rem',
+            alignItems: 'center'
+          }}>
+          <img
+            src="/images/palestine_flag.png"
+            style={{
+              borderRadius: '10%'
+            }}
+          />
+          <h1
+            style={{
+              fontSize: isUnder600 ? '0.7rem' : '1rem'
+            }}>
+            Donate to support Palestine X
+            <span
+              style={{
+                textDecoration: 'underline',
+                // move underline little bit downwards
+                textUnderlineOffset: '0.2rem',
+                margin: '0 0.4rem'
+              }}>
+              Al Khidmat Foundation
+            </span>
+          </h1>
+        </div>
+        {!props.hideMessage && (
+          <div
+            style={{
+              fontSize: isUnder600 ? '0.8rem' : '0.85rem',
+              color: 'var(--muted-text)',
+              textAlign: 'center',
+              fontStyle: 'italic'
+            }}>
+            {'"' + quranicVerses[verseIndex] + '"'} -- {'Al-Qur’an'}
+          </div>
+        )}
+      </a>
+    </motion.div>
+  );
+
   return (
     <Flex
       p={2}

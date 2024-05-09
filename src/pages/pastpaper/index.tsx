2df7b578fdee8bf912ad21c6cf5163aaa84198f8
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-05-09 22:02:10 +0500

    feat: added past paper page (#37)

--- src/pages/pastpaper/index.tsx
+++ src/pages/pastpaper/index.tsx
@@ -1,47 +1,16 @@
-import PastPaper from '~/components/pastpaper/pastpaper';
-
 import { GetServerSidePropsContext } from 'next';
-import { doc, getDoc, getDocs } from 'firebase/firestore';
-import { pastPapersCol, pastPapersInputCol } from '~/lib/firebase';
-import { PastPaperDocType } from '~/types/typedef';
-
-import { fromFirebaseTimeStamp } from '~/lib/util';
-import AdminLayout from '~/components/admin/layout';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const subjectDocRef = doc(pastPapersInputCol, 'subjects');
-
-  const [subjectsSnapShot, pastPapersSnapShot] = await Promise.all([
-    getDoc(subjectDocRef),
-    getDocs(pastPapersCol)
-  ]);
-
-  const data = subjectsSnapShot.data();
-
-  const pastPapers = pastPapersSnapShot.docs.map((data) => ({
-    ...data.data(),
-    uploadedAt: fromFirebaseTimeStamp(data.data().uploadedAt).toString()
-  }));
-
+  // todo: Get all subjects here
   return {
-    props: {
-      data,
-      pastPapers
-    }
+    props: {}
   };
 }
 
 interface GetStaticPropsReturnType {
-  data: { data: { [department: string]: Array<string> } };
-  pastPapers: Array<PastPaperDocType>;
+  data: {};
 }
 
-export default function PastPaperPage({ data, pastPapers }: GetStaticPropsReturnType) {
-  return (
-    <>
-      <AdminLayout>
-        <PastPaper staticData={data.data} pastPapers={pastPapers} />
-      </AdminLayout>
-    </>
-  );
+export default function PastPaperPage({}: GetStaticPropsReturnType) {
+  return <>Past Papers</>;
 }
